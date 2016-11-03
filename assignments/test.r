c9e27f360da03ffa33c723dd8d2d4510d35e645e
LSaldyt <lucassaldyt@yahoo.com>
2016-11-03 13:10:54 -0600

    Adds qqplot script in r

---
+++ assignments/test.r
@@ -0,0 +1,6 @@
+library(nortest)
+
+y <- CO2[,5]
+
+qqnorm(y); qqline(y)
+
--- scripts/read_systems.py
+++ scripts/read_systems.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 from fetch import fetch
 
-#sheetId = '10JbiYwJH4eJHjSBS1mPj7lQ6RpZaP0PN1HgXu_gVKss'
 sheetId = '13_OfCVf0pI1pDAV1GYtATZt2slUexAgonN7VGFKJprk'
 
 def main():
@@ -11,7 +10,7 @@ def main():
         name    = systemdata[2]
         x, y, z = tuple(systemdata[-4:-1])
         coords  = '(%s, %s, %s)' % (x, y, z)
-        print('System: %s %s' % (name.rjust(20), coords.ljust(5)))
+        print('%s %s' % (name.rjust(20), coords.ljust(5)))
 
 
 if __name__ == '__main__':
