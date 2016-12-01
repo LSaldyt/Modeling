2377e1d87293a754efc40f990eb51923a141c039
LSaldyt <lucassaldyt@yahoo.com>
2016-12-01 10:24:49 -0700

    Adds boxplots for all statistics

---
+++ assignments/groupsize/groupsize_boxplots.R
@@ -0,0 +1,27 @@
+
+boxPlot <- function(data)
+{
+    boxplot(data,
+            las   = 2, 
+            names = c('Min + Max', 
+                      '2 * Mean', 
+                      '2 * Median', 
+                      'First Quartile + Third Quartile - Min', 
+                      'Max - Min', 
+                      '(2 * Median) - Min',
+                      'Max + (Standard Deviation / 2) - Min'),
+                col=(c("#ff9900")))
+}
+
+plotConfidence <- function(filename)
+{
+    estimates <- read.csv(filename, header=FALSE)
+    t(estimates)
+    print(estimates)
+    boxPlot(estimates)
+}
+
+plotConfidence("estimates.csv")
+plotConfidence("estimates_shifted.csv")
+plotConfidence("estimates_skewed.csv")
+
